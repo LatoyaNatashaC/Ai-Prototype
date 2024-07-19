d4f75801f91abc6365612ee3955151024668a035
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-07-18 22:13:10 -0700

    improved user experience

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ function generatePoem(event) {
   let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
   let prompt = `generate a poem about ${userInput.value}`;
   let context =
-    "user instructions: you are a romantic poem expert and love to write short poems. your mission is to generate a four line poem in basic HTML and seperate each line with a <br />. be sure to follow the user instructions. sign the poem with 'SheCodes Ai' inside a <strong> element";
+    "user instructions: you are a romantic poem expert and love to write short poems. your mission is to generate a four line poem in basic HTML and seperate each line with a <br />. be sure to follow the user instructions. sign the poem with 'SheCodes Ai' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Poem about ${userInput.value}</div>`;
 
   console.log("generating poem");
   console.log(`prompt: ${prompt}`);
